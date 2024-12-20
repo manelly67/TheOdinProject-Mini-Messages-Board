e4588b9f1f1a939819e54b35860197d32faa3411
manellylopez <manellylopezqqq@gmail.com>
2024-12-20 09:38:08 -0500

    form validation

--- routes/messagesRoutes.js
+++ routes/messagesRoutes.js
@@ -1,9 +1,15 @@
 // messagesRoutes.js
 const express = require("express");
 const router = express.Router();
+const { body, validationResult } = require("express-validator");
 const arrays = require('../arrays');
 
 const { links, messages } = arrays;
+
+const validateUser = [
+  body("user").trim().notEmpty().escape(),
+  body("text").trim().notEmpty().escape(),
+];
 
 // new messages routes
 
@@ -11,13 +17,27 @@ router.get("/", (req, res) => {
     res.render("new", { links: links, title:links[1].text, messages: messages});
 });
 
+/*  sintaxis sin utilizar validacion
 router.post("/", (req, res) => {
-  
   const { user, text } = req.body ;
   messages.push({ text: text, user: user, added: new Date() });
   res.redirect("/");
- 
 });
+*/
+
+router.post("/", [
+  validateUser,
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      console.log('error - not valid');
+    }
+    const { user, text } = req.body;
+    messages.push({ text: text, user: user, added: new Date() });
+    res.redirect("/");
+  }
+]);
+
 
 router.get("/:id", (req, res) => {
     const id = req.params.id;
